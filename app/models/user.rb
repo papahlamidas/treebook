dd1bc363ddafc77cf03f4cd53555e7a7156c1eba
papahlamidas <papahlamidas@hotmail.com>
2013-07-22 15:14:09 +0300

    Updated frontend for the new authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-                  :first_name, :last_name, :profile_name  
+                  :first_name, :last_name, :profile_name 
+
+   has_many :statuses
+
+   def full_name
+first_name + " " +last_name
+   end
 end
